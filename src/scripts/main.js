9a01169f0f79cc2c93755ec2d02ad6f8b4d6cafa
yuliana111007-debug <yuliana111007@gmail.com>
2026-02-11 23:40:34 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,11 +4,20 @@ const populationEl = document.querySelectorAll('.population');
 const totalRes = document.querySelector('.total-population');
 const averageRes = document.querySelector('.average-population');
 const populationArray = Array.from(populationEl).map((el) => {
-  return Number(el.textContent.replace(/,/g, ''));
+  const clean = el.textContent.replace(/,/g, '');
+  const n = Number(clean);
+
+  return n;
 });
+const filteredArr = populationArray.filter((n) => !isNaN(n));
 
-const total = populationArray.reduce((acc, curr) => acc + curr, 0);
-const average = Math.round(total / populationArray.length);
+if (filteredArr.length === 0) {
+  totalRes.textContent = '0';
+  averageRes.textContent = '0';
+}
 
-totalRes.textContent = total.toLocaleString();
-averageRes.textContent = average.toLocaleString();
+const total = filteredArr.reduce((acc, curr) => acc + curr, 0);
+const average = Math.round(total / filteredArr.length);
+
+totalRes.textContent = total.toLocaleString('en-En');
+averageRes.textContent = average.toLocaleString('en-EN');
